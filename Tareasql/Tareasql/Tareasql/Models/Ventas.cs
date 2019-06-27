34adf2d4d04dbee340177a72374c87f2b5861ced
Nabil Galeb <nabilgaleb@hotmail.com>
2019-06-27 15:16:38 -0400

    segundo commit ventas

---
+++ Tareasql/Tareasql/Tareasql/Models/Ventas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SQLite;
+
+namespace Tareasql.Models
+{
+    [Table("ventas")]
+    public class Ventas
+    {
+        [PrimaryKey, AutoIncrement]
+        public int VentaId { get; set; }
+
+        [MaxLength(70)]
+        public string Producto { get; set; }
+
+        [NotNull]
+        public int Cantidad { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+    }
+}
